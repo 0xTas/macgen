8ccaeb0e6d9853f0b981dc68fc59a2cbaf1c20e9
0xTas <admin@0xTas.dev>
2023-03-16 22:34:16 -0700

    Now generates valid OUI addresses

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,84 @@
-use rand::{thread_rng, Rng};
+use rand::{seq::SliceRandom, thread_rng, Rng};
 
 fn main() {
-    let fake_addr: u64 = thread_rng().gen_range(0x100000000000..=0xffffffffffff);
-    let fake_addr_str = format!("{:2X}", fake_addr);
+    let oui_prefixes: Vec<&str> = vec![
+        "00:0E:F6", "00:08:28", "84:7A:88", "D8:A2:5E", "00:30:0B", "00:26:0C", "A4:C5:4E",
+        "94:86:CD", "E0:35:60", "00:19:BC", "70:01:36", "FC:1F:C0", "00:E0:DE", "00:07:19",
+        "00:1B:AF", "00:24:27", "28:4F:CE", "00:22:A0", "74:40:BB", "28:E7:94", "C4:93:00",
+        "30:A2:20", "00:17:88", "02:5B:76", "2C:F4:32", "40:16:7E", "40:B0:76", "5C:A6:E6",
+        "60:E3:27", "68:9A:87", "62:5C:65", "68:9A:87", "A0:8C:FD", "B8:BB:AF", "F0:F2:74",
+        "F0:35:75", "F2:34:11", "CC:40:D0", "20:4E:7F", "AC:37:43", "04:0E:C2", "18:2B:05",
+        "00:04:D2", "24:B6:FD", "F0:46:3B", "00:50:FF", "00:0B:2A", "00:1F:ED", "00:22:FB",
+        "CC:3F:EA", "00:02:BF",
+    ];
 
-    let substrings = fake_addr_str
-        .as_bytes()
-        .chunks(2)
-        .map(std::str::from_utf8)
-        .collect::<Result<Vec<&str>, _>>()
-        .expect("Expected valid chunks!");
+    let iters = if std::env::args().any(|arg| arg.parse::<u32>().is_ok()) {
+        let arg_num = std::env::args()
+            .find(|arg| arg.parse::<u32>().is_ok())
+            .expect("Pre-checked for numeric arg.")
+            .parse::<u32>()
+            .expect("Already verified as parsable arg.");
 
-    let formatted_addr = substrings.join(":");
+        if arg_num == 0 {
+            1
+        } else {
+            arg_num
+        }
+    } else {
+        1
+    };
 
     if std::env::args().any(|arg| arg == "-n") {
-        println!("wgy");
-        println!("{}", formatted_addr);
+        for _ in 0..iters {
+            let mut rng = thread_rng();
+
+            let fake_addr: u64 = thread_rng().gen_range(0x100000..=0xffffff);
+            let fake_addr_str = format!("{:2X}", fake_addr);
+
+            let substrings = fake_addr_str
+                .as_bytes()
+                .chunks(2)
+                .map(std::str::from_utf8)
+                .collect::<Result<Vec<&str>, _>>()
+                .expect("Expected valid chunks!");
+
+            let formatted_addr = substrings.join(":");
+
+            print!(
+                "{}:{} ",
+                oui_prefixes
+                    .choose(&mut rng)
+                    .expect("Vec should not be empty."),
+                formatted_addr
+            );
+        }
     } else if std::env::args()
         .any(|arg| arg.to_lowercase() == "-h" || arg.to_lowercase() == "--help")
     {
-        println!("plss");
-        println!("Usage: ./macgen [-n (appends newline)]");
+        println!("Usage: ./macgen [-n (won't append newline)] [num (e.g 5)]");
     } else {
-        println!("whyyy");
-        print!("{}", formatted_addr);
+        for _ in 0..iters {
+            let mut rng = thread_rng();
+
+            let fake_addr: u64 = thread_rng().gen_range(0x100000..=0xffffff);
+            let fake_addr_str = format!("{:2X}", fake_addr);
+
+            let substrings = fake_addr_str
+                .as_bytes()
+                .chunks(2)
+                .map(std::str::from_utf8)
+                .collect::<Result<Vec<&str>, _>>()
+                .expect("Expected valid chunks!");
+
+            let formatted_addr = substrings.join(":");
+
+            println!(
+                "{}:{}",
+                oui_prefixes
+                    .choose(&mut rng)
+                    .expect("Vec should not be empty."),
+                formatted_addr
+            );
+        }
     };
 }
