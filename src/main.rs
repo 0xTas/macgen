2b08662ed2ae920611b01cfc470d89faf24af9bb
0xTas <admin@0xTas.dev>
2023-03-16 21:12:27 -0700

    Added -n (appends newline) and --help

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,22 @@ fn main() {
     let fake_addr: u64 = thread_rng().gen_range(0x100000000000..=0xffffffffffff);
     let fake_addr_str = format!("{:2X}", fake_addr);
 
-    let substrings = fake_addr_str.as_bytes()
+    let substrings = fake_addr_str
+        .as_bytes()
         .chunks(2)
         .map(std::str::from_utf8)
         .collect::<Result<Vec<&str>, _>>()
         .expect("Expected valid chunks!");
 
     let formatted_addr = substrings.join(":");
-    print!("{}", formatted_addr);
+
+    let mut args = std::env::args();
+
+    if args.any(|arg| arg == "-n") {
+        println!("{}", formatted_addr);
+    } else if args.any(|arg| arg.to_lowercase() == "-h" || arg.to_lowercase() == "--help") {
+        println!("Usage: ./macgen [-n (appends newline)]");
+    } else {
+        print!("{}", formatted_addr);
+    };
 }
